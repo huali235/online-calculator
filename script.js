4fcfcbd6a0211433b95f1762e273301ec97c4958
Hussain Ali <huali235@gmail.com>
2023-05-06 18:59:00 -0400

    2 number operations functional

--- script.js
+++ script.js
@@ -9,39 +9,73 @@ const equals = document.querySelector(".equals");
 const operationString = document.querySelector(".operation-string");
 const total = document.querySelector(".total");
 let numValue = "";
-let num1 = "";
-
+let tempNum = "";
+let numArray = [];
+let currentOperator = "";
+let isSecondOperand = false;
 /// Functions
 
 const init = function () {
+  numValue = "";
+  currentOperator = "";
+  isSecondOperand = false;
   total.textContent = "";
   operationString.textContent = "";
+  tempNum = "";
+  numArray = [];
 };
 
-const displayNum = function () {
+const appendFirstNumber = function () {
   numbers.forEach((num) => {
+    isSecondOperand = false;
     num.addEventListener("click", () => {
       numValue = "";
       numValue += num.innerHTML;
       total.textContent += numValue;
-      num1 += numValue;
+      if (isSecondOperand) {
+        tempNum += numValue;
+        numArray[1] = tempNum;
+      } else {
+        tempNum += numValue;
+        numArray[0] = tempNum;
+      }
     });
   });
 };
 
-const displayOperator = function () {
+const chooseOperation = function () {
   operators.forEach((operator) => {
-    operator.classList.add(".operator-style");
     operator.addEventListener("click", () => {
-      num1 += operator.innerHTML;
-      operationString.textContent += num1;
+      numArray.push(tempNum);
+      currentOperator = operator.innerHTML;
+      operationString.textContent +=
+        numArray.length > 1
+          ? ` ${numArray[1]} ${operator.innerHTML}`
+          : ` ${numArray[0]} ${operator.innerHTML}`;
       total.textContent = "";
+      tempNum = "";
+      isSecondOperand = true;
+      console.log(numArray);
     });
   });
-  numValue = "";
+};
+
+const calculateTotal = function () {
+  equals.addEventListener("click", () => {
+    numArray.push(tempNum);
+    let num1 = Number(numArray[0]);
+    let num2 = Number(numArray[1]);
+    let result;
+    if (currentOperator === "+") result = num1 + num2;
+    else if (currentOperator === "-") result = num1 - num2;
+    else if (currentOperator === "X") result = num1 * num2;
+    else if (currentOperator === "/") result = num1 / num2;
+    total.textContent = result;
+  });
 };
 
 init();
-displayNum();
-displayOperator();
+appendFirstNumber();
+chooseOperation();
+calculateTotal();
 clear.addEventListener("click", init);
